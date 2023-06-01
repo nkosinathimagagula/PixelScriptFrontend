4d36159710747a496ad82b5b98f8c43ead78c591
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-06-01 23:02:03 +0200

    add updated src files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { NavBar } from "./components/NavBar";
+import { LandingSection } from "./components/LandingSection";
+
+const App = () => {
+
+  return (
+    <>
+      <NavBar />
+      <LandingSection />
+    </>
+      
+  )
+}
+
+export default App
