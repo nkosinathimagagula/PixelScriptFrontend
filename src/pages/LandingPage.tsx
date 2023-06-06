3d2fb8f63ca5c2903379514fbe4a234404d0e9a7
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-06-06 13:16:29 +0200

    add landing page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import { NavBar } from "../components/NavBar";
+import { LandingSection } from "../components/LandingSection";
+
+
+export const LandingPage = () => {
+
+  return (
+    <>
+      <NavBar/>
+      <LandingSection />
+    </>
+  )
+}
