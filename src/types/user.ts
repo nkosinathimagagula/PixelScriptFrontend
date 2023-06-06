708ff16d3af210c9c40ad985623e09ca8d0b0b85
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-06-06 13:13:22 +0200

    add token and user types

---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+export type signupUserBase = {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export type signinUserBase = {
+    email: string;
+    password: string;
+}
