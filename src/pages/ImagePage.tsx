bd80fe5ddc786e59b7afc9dc16723b523f0d790c
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-06-06 13:16:53 +0200

    add image extraction  page

---
+++ src/pages/ImagePage.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../components/NavBar"
+import { ImageSection } from "../components/ImageSection"
+
+export const ImagePage = () => {
+    
+    return (
+        <>
+            <NavBar />
+            <ImageSection />
+        </>
+    )
+}
