2454bf1d01cc4bbce752120494a283740ee4cef4
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-06-06 13:15:28 +0200

    add signup and signin page

---
+++ src/pages/SigninPage.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../components/NavBar"
+import { Signin } from "../components/Signin"
+
+export const SigninPage = () => {
+
+    return (
+        <>
+            <NavBar />
+            <Signin/>
+        </>
+    )
+}
---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../components/NavBar"
+import { Signup } from "../components/Signup"
+
+export const SignupPage = () => {
+
+    return (
+        <>
+            <NavBar />
+            <Signup />
+        </>
+    )
+}
