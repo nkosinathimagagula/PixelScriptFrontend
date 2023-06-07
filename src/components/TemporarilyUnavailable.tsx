93b1efd547ddaaf9d12ba874109a71fad74898e6
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-06-07 23:29:18 +0200

    add temporary page

---
+++ src/components/TemporarilyUnavailable.tsx
@@ -0,0 +1,10 @@
+export const TemporarilyUnavailable = () => {
+
+    return (
+        <section className="bg-[#374151] w-full h-screen sm:pt-20 pt-16">
+            <div className="w-full h-full flex justify-center py-[30vh]">
+                <h2 className="text-[60px] text-[#e0fbfc]">Temporarily Unavailable.</h2>
+            </div>
+        </section>
+    )
+}
---
+++ src/pages/TemporaryPage.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../components/NavBar"
+import { TemporarilyUnavailable } from "../components/TemporarilyUnavailable"
+
+export const TemporaryPage = () => {
+
+    return (
+        <>
+            <NavBar />
+            <TemporarilyUnavailable />
+        </>
+    )
+}
