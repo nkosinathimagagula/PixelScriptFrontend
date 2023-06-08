b3a08d54fa07504d11e4227b1a118da4cd803c75
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-06-08 20:51:42 +0200

    update image section to use document container

--- src/components/ImageSection.tsx
+++ src/components/ImageSection.tsx
@@ -4,8 +4,7 @@ import { ThreeDots } from "react-loader-spinner";
 import { convertImage } from "../utils/imageUtils";
 import { extractText } from "../api/PSAPI";
 import { ExtractResponse } from "../types/text";
-import { DocumentViewer } from "./DocumentViewer";
-
+import { DocumentContainer } from "./DocumentContainer";
 
 
 export const ImageSection = () => {
@@ -26,21 +25,6 @@ export const ImageSection = () => {
 
         const imageFile: File | null = uploadedImage;
         extractText(imageFile, setResponse);
-    }
-
-    const handleDownloadOnClick = () => {
-        const filename: string = response ? "PS" + response.date + "~" + response.id + ".doc" : '';
-        
-        const element: string = (document.getElementById("document") as HTMLInputElement).innerText;
-        const formatted: string = element.replace(/[\u00A0\u180E\u2000-\u200B\u202F\u205F\u3000\uFEFF]/g, "").replace(/ \n/g, " ");
-
-        
-        const downloadLink: HTMLAnchorElement = document.createElement('a');
-
-        downloadLink.setAttribute("download", filename);
-        downloadLink.setAttribute("href", "data:" + "text/docx;" + "charset=utf-8," + encodeURIComponent(formatted))
-
-        downloadLink.click();
     }
 
     useEffect(() => {
@@ -136,37 +120,7 @@ export const ImageSection = () => {
                     </form>
                 </div>
 
-                <div className={response ? 'bg-[#293241] flex flex-col w-full overflow-auto h-full rounded-2xl p-5 gap-3 scrolling' : 'hidden'}>
-                    <h2 className="text-[30px] text-[#e0fbfc]">Extracted Text [Bionic Reading]</h2>
-                    <div className="flex sm:flex-row flex-col w-full sm:gap-20 gap-10">
-                        <DocumentViewer response={response} />
-                        <div className="sm:w-[30vw] w-full justify-center">
-                            <h2 className="text-[30px] text-[#e0fbfc]">Info</h2>
-                            <p className="text-white text-[16px] font-light tracking-widest leading-[30px] pt-5 sm:mr-10">
-                                Below you can download the extracted text as a word document which you can easily edit. 
-                                The text in the document does not have the bionic reading method appied.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-row w-full gap-5 pt-5 justify-center">
-                    <button
-                        onClick={() => {
-                            setResponse(null);
-                            setImage("");
-                        }}
-                            className="bg-[#00ffef] text-[#374151] rounded-lg w-24 h-10 hover:bg-[#77fff7] button-hover"
-                        >
-                           Back
-                        </button>
-                        <button
-                        onClick={handleDownloadOnClick}
-                            className="bg-[#00ffef] text-[#374151] rounded-lg w-24 h-10 hover:bg-[#77fff7] button-hover"
-                        >
-                            Download
-                        </button>
-                    </div>
-                </div>
+                <DocumentContainer response={response} setImage={setImage} setResponse={setResponse}/>
             </div>
         </section>
     )
